70e0c725947fa7e2387ccb5469fb26c7acd9561b
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2024-08-11 11:35:07 +0200

    Creating the flashcard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import './App.css'
+import './App.css';
+
+import Flashcards from './components/Flashcards';
 
 function App() {
 
   return (
     <>
       <div className="text-4xl">Hell</div>
+      <Flashcards />
     </>
   )
 }
---
+++ src/components/Flashcards.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface FlashcardsProps {
+
+}
+
+const Flashcards: FC<FlashcardsProps> = ({}) => {
+  return <div>Test</div>
+}
+
+export default Flashcards;
